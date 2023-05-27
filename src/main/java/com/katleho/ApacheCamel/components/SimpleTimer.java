9c9cb4d8ca07f53611b78aed880a9dc52102a2cf
katleho <katlehomofokeng@protonmail.com>
2023-05-27 12:38:16 +0200

    SimpleTimer class

---
+++ src/main/java/com/katleho/ApacheCamel/components/SimpleTimer.java
@@ -0,0 +1,12 @@
+package com.katleho.ApacheCamel.components;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleTimer extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
