93796d77d5270ce9201ac55a51d390e0e27acf92
katleho <katlehomofokeng@protonmail.com>
2023-05-27 12:25:44 +0200

    first commit

---
+++ src/test/java/com/katleho/ApacheCamel/ApacheCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katleho.ApacheCamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
